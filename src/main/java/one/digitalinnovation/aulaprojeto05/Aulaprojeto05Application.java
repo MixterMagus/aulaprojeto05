71366fac101fa3d0ff68b4a35c27cb0e85177223
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-04-16 15:07:17 -0300

    Construindo projeto aula5
    Commit inicial aula5 Inter Java Developer

---
+++ src/main/java/one/digitalinnovation/aulaprojeto05/Aulaprojeto05Application.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.aulaprojeto05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aulaprojeto05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aulaprojeto05Application.class, args);
+	}
+
+}
